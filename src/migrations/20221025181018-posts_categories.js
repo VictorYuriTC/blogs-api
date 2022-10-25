203485b6581d0cef8d5a7af84ea513db07c83d41
Victor Yuri <victoryuritc@yahoo.com>
2022-10-25 16:01:37 -0300

    ADD posts_categories table with references

--- src/migrations/20221025181018-posts_categories.js
+++ src/migrations/20221025181018-posts_categories.js
@@ -2,20 +2,29 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('posts_categories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id'
+        },
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id'
+        }
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('posts_categories')
   }
 };
