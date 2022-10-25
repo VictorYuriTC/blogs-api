2523c048ef42fc6e5ca999ae33290ae985249620
Victor Yuri <victoryuritc@yahoo.com>
2022-10-25 15:54:10 -0300

    ADD blog_posts table with users references

--- src/migrations/20221025181011-blog_posts.js
+++ src/migrations/20221025181011-blog_posts.js
@@ -2,12 +2,35 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      published: {
+        type: 'TIMESTAMP'
+      },
+      updated: {
+        type: 'TIMESTAMP'
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
