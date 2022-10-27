3d5b7864ce05398e401485cf517ce2aac93b62d3
Victor Yuri <victoryuritc@yahoo.com>
2022-10-26 22:57:45 -0300

    DEL old validateJWT. ADD new validateJWT

--- src/middlewares/authentications/validateJWT.js
+++ src/middlewares/authentications/validateJWT.js
@@ -5,27 +5,13 @@ require('dotenv/config');
 
 const secret = process.env.JWT_SECRET;
 
-const validateJWT = async (req, res, next) => {
-  const token = req.header('Authorization');
-
-  if (!token) {
-    return res.status(401).json({ error: 'Token não encontrado ' });
-  }
-
+const validateJWT = async (token) => {
   try {
-    const decoded = jwt.verify(token, secret);
-
-    const user = await userService.addNewUserByEmail(decoded.data.email);
-
-    if (!user) {
-      return res.status(401).json({ message: 'Erro ao procurar usuário do token' });
-    }
-
-    req.user = user;
-
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Eu hein' });
+    const { data } = jwt.verify(token, secret);
+    return data;
+  } catch (_e) {
+    const error = new Error('Invalid token');
+    throw error;
   }
 };
 
